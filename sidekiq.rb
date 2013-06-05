102dfc85d482035d8f54367f38221bd4e77b5092
Aaron Cruz <aaron@aaroncruz.com>
2013-06-05 10:24:56 +0200

    current_path instead of release_path sidekiq

--- sidekiq.rb
+++ sidekiq.rb
@@ -1,6 +1,6 @@
 namespace :sidekiq do
   task :symlink, roles: :app do
-    run "ln -nfs #{shared_path}/config/sidekiq.yml #{release_path}/config/sidekiq.yml"
+    run "ln -nfs #{shared_path}/config/sidekiq.yml #{current_path}/config/sidekiq.yml"
   end
   before "deploy:start",   "sidekiq:symlink"
   before "deploy:restart", "sidekiq:symlink"
